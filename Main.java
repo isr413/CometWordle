a723d1d6ea26821806261d75b31c9fd7ee0a0386
isr413 <isr413@utulsa.edu>
2024-01-18 22:13:42 +0000

    selects random word from dictionary

--- Main.java
+++ Main.java
@@ -1,36 +1,13 @@
 public class Main {
-  public static void main(String[] args) throws java.io.FileNotFoundException {
-    // System.out.println("Hello world!");
-    java.util.HashSet<String> words = toSet("wordle-La.txt");
-    words.addAll(toSet("wordle-Ta.txt"));
+  public static void main(String[] args) {
+    try {
+      java.util.HashSet<String> words = CreateDictionary.toSet("dictionary.txt");
+      java.util.ArrayList<String> list = new java.util.ArrayList<>(words);
 
-    java.util.ArrayList<String> list = new java.util.ArrayList<>(words);
-    java.util.Collections.sort(list);
-    
-    java.io.PrintWriter writer = new java.io.PrintWriter(
-        new java.io.File("resources", "dictionary.txt"));
-    
-    for (String word : list) {
-      writer.println(word);
-      writer.flush();
+      int randomWord = new java.util.Random().nextInt(words.size());
+      System.out.println(list.get(randomWord));
+    } catch (java.io.FileNotFoundException e) {
+      System.out.println(e);
     }
-    
-    writer.close();
-    System.out.println(words.size());
-  }
-
-  public static java.util.HashSet<String> toSet(
-      String source) throws java.io.FileNotFoundException {
-    java.util.HashSet<String> set = new java.util.HashSet<String>();
-    
-    java.util.Scanner scanner = new java.util.Scanner(
-        new java.io.File("resources", source));
-    
-    while (scanner.hasNextLine()) {
-      set.add(scanner.nextLine());
-    }
-    
-    scanner.close();
-    return set;
   }
 }
